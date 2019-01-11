4839804a4c5523429527c2af95f5fb32faa5007f
Evan McPeters <evan.mcpeters@appfolio.com>
2019-01-11 10:39:18 -0800

    Task 1 - Client side only display image using React

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,10 +1,73 @@
-/* eslint no-console:0 */
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-//
-// To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
-// layout file, like app/views/layouts/application.html.erb
+import React, {Component} from 'react'
+import ReactDOM from 'react-dom'
+import PropTypes from 'prop-types'
 
-require('delete_me');
+class URLForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      url: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({url: event.target.value});
+  }
+
+  handleSubmit(event) {
+    this.props.onURLForm(this.state.url);
+    this.setState({url: ''});
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div className="m-5">
+        <form onSubmit={this.handleSubmit}>
+          <div className="form-group">
+          <label>Image URL:</label>
+          <input type="text" className="form-control" aria-describedby="urlHelp" placeholder="Enter URL" value={this.state.url} onChange={this.handleChange} />
+          <small id="urlHelp" className="form-text text-muted">Enter an image URL.</small>
+          <input type="submit" className="mt-3" value="Submit" />
+          </div>
+        </form>
+      </div>
+    )
+  }
+}
+
+const DisplayImage = props => (
+  <div className="m-5">
+    <img src={props.url} />
+  </div>
+)
+
+class ImageSharer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      url: ''
+    };
+  }
+
+  handleURL = (urlValue) => {
+    this.setState({url: urlValue});
+  }
+
+  render() {
+    return (
+      <div>
+        <URLForm onURLForm={this.handleURL}/>
+        <DisplayImage url={this.state.url}/>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(
+<ImageSharer />,
+  document.getElementById('test')
+)
